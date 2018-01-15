c25803c5891d43a9c9739829bf2750894000a2dc
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-15 17:56:50 +0530

    adding register new user functionality

---
+++ FindMyMate/FindMyMate-lambda.js
@@ -0,0 +1,31 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.put({
+		TableName: 'hs_user_profile',
+		Item: {
+            contact_number: event.contact_number,
+            age: event.age,
+            username: event.username,
+            email: event.email,
+            first_name: event.first_name,
+            last_known_lat: event.last_known_lat,
+            last_updated_timestamp: event.last_updated_timestamp,
+            last_known_long: event.last_known_long,
+            password: event.password,
+            last_name: event.last_name,
+            gender: event.gender,
+            interested_in: event.interested_in,
+            user_avatar: event.user_avatar
+        }
+	}, function (err, data) {
+		if (err) {
+		callback(err, null);
+		} else {
+			 callback(null, event);
+		}
+	});
+
+
+	callback(null, 'Successfully executed');
+}
