5c04eb017906e0598291ec5f5486a50873f1e9a6
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-18 10:39:49 +0530

    signIn

--- FindMyMate/getUserData-lambda.js
+++ FindMyMate/getUserData-lambda.js
@@ -2,13 +2,13 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	ddb.get({
-		TableName: 'hs_user',
-		Key: { username: event.queryStringParameters.username }
+		TableName: 'hs_user_profile',
+		Key: { 'username': event.queryStringParameters.username  }
 	}, function (err, data) {
 		if (err) {
-			  callback(err, null);
+		  callback(err, null);
 		} else {
-			 let response = {
+		 let response = {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
